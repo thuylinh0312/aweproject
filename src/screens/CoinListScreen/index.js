792170862b42a32fd3906fabb6a403f032de777b
Thuy Linh <thlinh0312@gmail.com>
2022-05-09 09:46:18 +0700

    update

--- src/screens/CoinListScreen/index.js
+++ src/screens/CoinListScreen/index.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react'
-import {View, Text, StyleSheet, FlatList} from 'react-native'
+import {View, Text, StyleSheet, FlatList, Image} from 'react-native'
 import axios from 'axios'
 
 // sau khi render => goi API, lay data => set data vao state,
@@ -17,13 +17,13 @@ export const CoinListScreen = () => {
         //         response.json().then(data => setList(data.data))
         //     })
 
-        axios.get('https://web-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&sort_dir=desc&limit=100')
+        axios.get('https://web-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&sort_dir=desc&limit=20')
             .then(({data}) => {
                 setList(data.data)
             })
     }, []) // array dependencies
 
-    console.log('list12312312321312312312312312', list)
+    //console.log('list12312312321312312312312312', list)
 
 
     console.log('Chay return')
@@ -32,7 +32,24 @@ export const CoinListScreen = () => {
             <FlatList
                 data={list}
                 renderItem={({item, index}) => {
-                    return <Text>{item.name}</Text>
+                    return (
+                        <View style = {styles.container}>
+                            <Image 
+                                style={{width: 30, height: 30, marginRight: 15}}
+                                source={{uri: `https://s2.coinmarketcap.com/static/img/coins/64x64/${item.id}.png?_=5bcdbc6`}}
+                            /> 
+                            <View>
+                                <Text style={{fontSize: 11 , fontWeight:"bold" }}>{item.name}</Text>
+                                <View style = {{flexDirection: "row"}}>
+                                    <Text style={{width: "25%", fontSize: 8 , backgroundColor: "lightgray"}}>{item.cmc_rank}</Text>
+                                    <Text style={{fontSize: 8 }}>{item.symbol}</Text>
+                                </View>
+                            </View>
+                            <Text style={{fontSize: 11 }}>đ{item.quote.USD.price*22.95250}</Text>
+                        </View>
+                        
+                    )
+                        
                 }}
             />
         </View>
@@ -41,6 +58,11 @@ export const CoinListScreen = () => {
 
 const styles = StyleSheet.create({
     wrapper: {
-        flex: 1
+        flex: 1, 
+    },
+    container:{
+        flexDirection: "row",
+        
+
     }
 })
