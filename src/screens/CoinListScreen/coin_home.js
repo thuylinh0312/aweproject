c8261c1e6a0c106be9e15fcf45793ec09ad94a0a
Thuy Linh <thlinh0312@gmail.com>
2022-05-16 10:50:24 +0700

    update

--- src/screens/CoinListScreen/coin_home.js
+++ src/screens/CoinListScreen/coin_home.js
@@ -3,34 +3,30 @@ import {Text, View, StyleSheet, TouchableOpacity} from "react-native"
 import { delayPromise } from "../../utils/common";
 
 // setTimeout, setInterval
-
-// const a = () => {
-//     return alert(123123)
-// }
-
-// const sum = (a, b) => {
-//     return a + b
-// }
-
-// a()
-
-// const tong = sum(1, 2)
-
 // xử lý bất đồng bộ: asynchorous
-
 // Promise
 
 export const CoinHome = ({navigation})=> {
+    const [check, setCheck] = useState(false)
+    const [minute, setMinute] = useState(0)
     const [count, setCount] = useState(0)
     const interval = useRef()
 
-
+   
     const startCount = () => {
         interval.current = setInterval(() => {
             setCount(count => count + 1)
+            if (count === 59){
+                setCount(0);
+                setMinute(minute => minute + 1)
+            }
         }, 1000)
     }
-
+    let time = {minute: minute, second: count}
+    const reset = () => {
+        setCount(0);
+        setMinute(0);
+    }
     // const callDelay = () => {
     //     delayPromise(5000)
     //         .then(() => {
@@ -44,52 +40,62 @@ export const CoinHome = ({navigation})=> {
     //         alert('123123123')
     // }
 
-    const callDelay = async () => {
-        try {
-            const data = await delayPromise(5000)
-            //,,,,,,,,,
-            alert(data)
+    // const callDelay = async () => {
+    //     try {
+    //         const data = await delayPromise(5000)
+    //         //,,,,,,,,,
+    //         alert(data)
 
-            ///////
-            /////
-        } catch (e) {
-            console.log(e)
-        } finally {
-            console.log('finallllllllllllllllyyy')
-        }
-    }
+    //         ///////
+    //     } catch (e) {
+    //         console.log(e)
+    //     } finally {
+    //         console.log('finallllllllllllllllyyy')
+    //     }
+    // }
 
 
     useEffect(() => {
-        callDelay()
-
-        console.log('Chay 1 lan duy nhat') 
-        // const timeout = setTimeout(() => {
-        //     setCount(0)
-        // }, 10000)
-
+    // callDelay()
+        if (check === true){
+            startCount()
+        }
         
-        startCount()
-
-        // setTimeout(() => {
-        //     clearInterval(interval)
-        //     clearTimeout(timeout)
-        // }, 7000) 
-
         return () => {
             clearInterval(interval.current)
         }
-    }, [])
+    }, [check, count])
 
     return (
-        <View style={{flexDirection: 'row', alignItems: 'center'}}>
-            <Text>{count}</Text>
-            <TouchableOpacity style={{marginLeft: 100}} onPress={() => clearInterval(interval.current)}>
-                <Text>STOP</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={{marginLeft: 100}} onPress={startCount}>
-                <Text>RESUME</Text>
-            </TouchableOpacity>
+        <View style={{ flex: 1, }}>
+            <View style = {{backgroundColor: "white", padding: 10, alignItems: "center"}}>
+                <Text >{minute} : {count}</Text>
+                
+            </View>
+           
+            <View style={{ flexDirection: "row", justifyContent: "space-around", marginTop:10}}>
+                <TouchableOpacity 
+                    style = {styles.button}
+                    onPress={() => setCheck(!check)}>
+                    {check === false ? <Text>Bắt đầu</Text> : <Text>Tạm dừng</Text>}
+                </TouchableOpacity>
+                <TouchableOpacity  
+                    style = {styles.button}
+                    onPress={() => reset()}
+                >
+                    {check === false ? <Text>Đặt lại</Text> : <Text>Vòng chạy</Text>}
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
+ const styles = StyleSheet.create({
+     button: {
+        backgroundColor: "white", 
+        padding: 10, 
+        borderColor: "black", 
+        borderWidth:2, borderRadius:10
+     }
+})
+
+ 
